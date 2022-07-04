8e66ed99b36c13a6558dc4327f88f99eb7214d89
Jonathan Klee <jonathan.klee@e.email>
2022-07-04 14:42:40 +0200

    Remove unusess imports

--- app/src/main/java/com/android/sapio/view/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/android/sapio/view/viewmodel/SearchViewModel.kt
@@ -5,11 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.android.sapio.model.Application
 import com.android.sapio.model.ApplicationsRepository
-import com.parse.ParseObject
-import com.parse.ParseQuery
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class SearchViewModel : ViewModel() {
 
