8219be19cbd6e4c461cad57c4da806261c97775a
Jonathan Klee <jonathan.klee@e.email>
2022-10-31 22:55:07 +0100

    Remove SafetyNet related feature

--- app/src/main/java/com/klee/sapio/view/ui/WarningFragment.kt
+++ app/src/main/java/com/klee/sapio/view/ui/WarningFragment.kt
@@ -7,18 +7,8 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.klee.sapio.R
 import com.klee.sapio.databinding.FragmentWarningBinding
-import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.GoogleApiAvailability
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.safetynet.SafetyNet
-import com.nimbusds.jose.JWSObject
 
 class WarningFragment : Fragment() {
-
-    companion object {
-        const val ERROR_SAFETYNET_DISABLED = 13
-        const val ERROR_SAFETYNET_PERMISSION_DENIED = 7
-    }
 
     private lateinit var mBinding: FragmentWarningBinding
 
@@ -29,39 +19,10 @@ class WarningFragment : Fragment() {
     ): View {
         mBinding = FragmentWarningBinding.inflate(layoutInflater)
 
-        mBinding.proceedButton.isEnabled = false
         mBinding.proceedButton.setOnClickListener {
             findNavController().navigate(R.id.action_warningFragment_to_chooseAppFragment)
         }
 
-        checkSafetyNet()
-
-
         return mBinding.root
-    }
-
-    private fun checkSafetyNet() {
-        if (GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(requireContext()) == ConnectionResult.SUCCESS) {
-            val nonce = System.currentTimeMillis().toString() + "Sapio"
-            SafetyNet.getClient(requireContext()).attest(nonce.toByteArray(), "AIzaSyAGXF8P1HbejJ2zthzsi2jd6nv6U86wA4g")
-                .addOnSuccessListener {
-                    val jws = JWSObject.parse(it.jwsResult)
-                    val json = jws.payload.toJSONObject()
-                    val ctsProfileMatch = json["ctsProfileMatch"].toString().toBoolean()
-                    val basicIntegrity = json["basicIntegrity"].toString().toBoolean()
-                    mBinding.proceedButton.isEnabled = !(ctsProfileMatch && basicIntegrity)
-                }
-                .addOnFailureListener { exception ->
-                    if (exception is ApiException) {
-                        when (exception.statusCode) {
-                            ERROR_SAFETYNET_PERMISSION_DENIED -> mBinding.proceedButton.isEnabled = false
-                            ERROR_SAFETYNET_DISABLED -> mBinding.proceedButton.isEnabled = true
-                            else -> mBinding.proceedButton.isEnabled = false
-                        }
-                    }
-                }
-        } else {
-            mBinding.proceedButton.isEnabled = true
-        }
     }
 }
