e4f77b31ef75b413191c510cf6f9c087b8277e10
Jonathan Klee <jonathan.klee@e.email>
2022-12-01 16:44:04 +0100

    Remove dead code

--- app/src/main/java/com/klee/sapio/data/InstalledApplicationsRepository.kt
+++ app/src/main/java/com/klee/sapio/data/InstalledApplicationsRepository.kt
@@ -1,7 +1,6 @@
 package com.klee.sapio.data
 
 import android.content.Context
-import android.content.Intent
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.graphics.drawable.Drawable
@@ -14,8 +13,6 @@ import javax.inject.Singleton
 class InstalledApplicationsRepository @Inject constructor() {
 
     fun getAppList(context: Context): List<InstalledApplication> {
-        val mainIntent = Intent(Intent.ACTION_MAIN)
-        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER)
         val apps = context.packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             apps.removeIf { x -> isSystemApp(x) }
