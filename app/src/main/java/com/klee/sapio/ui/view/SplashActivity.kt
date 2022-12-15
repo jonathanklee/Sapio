c00da47641801aa5d9a8b090ea791b6d4ecd08d1
Jonathan Klee <jonathan.klee@e.email>
2022-12-15 01:16:13 +0100

    Improve splash screen experience on Android 12

--- app/src/main/java/com/klee/sapio/ui/view/SplashActivity.kt
+++ app/src/main/java/com/klee/sapio/ui/view/SplashActivity.kt
@@ -1,16 +1,20 @@
 package com.klee.sapio.ui.view
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.klee.sapio.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition { Build.VERSION.SDK_INT >= 31 }
 
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         val binding = ActivitySplashBinding.inflate(layoutInflater)
