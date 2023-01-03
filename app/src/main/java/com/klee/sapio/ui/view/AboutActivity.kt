de12dec3c53ef9d6175aa9c149f118c4ee3c07dc
Jonathan Klee <jonathan.klee@e.email>
2023-01-03 17:42:54 +0100

    Display version in AboutActivity

--- app/src/main/java/com/klee/sapio/ui/view/AboutActivity.kt
+++ app/src/main/java/com/klee/sapio/ui/view/AboutActivity.kt
@@ -2,6 +2,7 @@ package com.klee.sapio.ui.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.klee.sapio.BuildConfig
 import com.klee.sapio.databinding.ActivityAboutBinding
 
 class AboutActivity : AppCompatActivity() {
@@ -12,6 +13,7 @@ class AboutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         mBinding = ActivityAboutBinding.inflate(layoutInflater)
+        mBinding.version.text = "v${BuildConfig.VERSION_NAME}"
         setContentView(mBinding.root)
     }
 }
