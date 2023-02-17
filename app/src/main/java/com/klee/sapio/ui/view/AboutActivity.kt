5f986140bd2082e3ba78ab303c9b43d6d82ebc7f
Jonathan Klee <jonathan.klee@e.email>
2023-02-17 15:46:09 +0100

    Link towards website in about activity

--- app/src/main/java/com/klee/sapio/ui/view/AboutActivity.kt
+++ app/src/main/java/com/klee/sapio/ui/view/AboutActivity.kt
@@ -1,5 +1,7 @@
 package com.klee.sapio.ui.view
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.klee.sapio.BuildConfig
@@ -9,11 +11,20 @@ class AboutActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityAboutBinding
 
+    companion object {
+        const val WEBSITE = "http://www.github.com/jonathanklee/sapio"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mBinding = ActivityAboutBinding.inflate(layoutInflater)
         mBinding.version.text = "v${BuildConfig.VERSION_NAME}"
+
+        mBinding.root.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(WEBSITE)))
+        }
+
         setContentView(mBinding.root)
     }
 }
