1be03eda93d996ba3051fb0071af1c75ece373bb
Jonathan Klee <jonathan.klee@e.email>
2023-12-08 20:49:00 +0100

    Implement MaterialYou design

--- app/src/main/java/com/klee/sapio/SapioApplication.kt
+++ app/src/main/java/com/klee/sapio/SapioApplication.kt
@@ -1,9 +1,14 @@
 package com.klee.sapio
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class SapioApplication : Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
 }
