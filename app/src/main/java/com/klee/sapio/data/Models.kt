197a8e9c88fa9b8fb9b311349a0159861a20944a
Jonathan Klee <jonathan.klee@e.email>
2023-12-16 17:05:40 +0100

    Create emojis constants

--- app/src/main/java/com/klee/sapio/data/Models.kt
+++ app/src/main/java/com/klee/sapio/data/Models.kt
@@ -180,12 +180,16 @@ data class Rating(val value: Int, val text: String) {
         const val AVERAGE = 2
         const val BAD = 3
 
+        private const val GREEN_CIRCLE_EMOJI =  0x1F7E2
+        private const val YELLOW_CIRCLE_EMOJI = 0x1F7E1
+        private const val RED_CIRCLE_EMOJI = 0x1F534
+
         fun create(rating: Int): Rating {
             return when (rating) {
-                GOOD -> Rating(GOOD, String(Character.toChars(0x1F7E2)))
-                AVERAGE -> Rating(AVERAGE, String(Character.toChars(0x1F7E1)))
-                BAD -> Rating(BAD, String(Character.toChars(0x1F534)))
-                else -> Rating(BAD, String(Character.toChars(0x1F534)))
+                GOOD -> Rating(GOOD, String(Character.toChars(GREEN_CIRCLE_EMOJI)))
+                AVERAGE -> Rating(AVERAGE, String(Character.toChars(YELLOW_CIRCLE_EMOJI)))
+                BAD -> Rating(BAD, String(Character.toChars(RED_CIRCLE_EMOJI)))
+                else -> Rating(BAD, String(Character.toChars(RED_CIRCLE_EMOJI)))
             }
         }
     }
