e4e9ed30c8403e963363f4c8192145f860f59250
Jonathan Klee <jonathan.klee@e.email>
2024-04-07 15:09:11 +0200

    Add ratings in AboutActivity

--- app/src/main/java/com/klee/sapio/ui/view/AboutActivity.kt
+++ app/src/main/java/com/klee/sapio/ui/view/AboutActivity.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.klee.sapio.BuildConfig
+import com.klee.sapio.data.Rating
 import com.klee.sapio.databinding.ActivityAboutBinding
 
 class AboutActivity : AppCompatActivity() {
@@ -20,6 +21,9 @@ class AboutActivity : AppCompatActivity() {
 
         mBinding = ActivityAboutBinding.inflate(layoutInflater)
         mBinding.version.text = "v${BuildConfig.VERSION_NAME}"
+        mBinding.greenIcon.text = Rating.create(Rating.GOOD).text
+        mBinding.yellowIcon.text = Rating.create(Rating.AVERAGE).text
+        mBinding.redIcon.text = Rating.create(Rating.BAD).text
 
         mBinding.root.setOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(WEBSITE)))
