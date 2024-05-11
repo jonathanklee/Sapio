1546a73277a67f3452dcd4bca8acba5b3ca7e138
Jonathan Klee <jonathan.klee@e.email>
2024-05-11 14:33:01 +0200

    Do not accept Fdroid apps

--- app/src/main/java/com/klee/sapio/ui/view/ChooseAppFragment.kt
+++ app/src/main/java/com/klee/sapio/ui/view/ChooseAppFragment.kt
@@ -1,18 +1,26 @@
 package com.klee.sapio.ui.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.klee.sapio.R
 import com.klee.sapio.databinding.FragmentChooseAppBinding
 import com.klee.sapio.data.InstalledApplication
 import dagger.hilt.android.AndroidEntryPoint
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.features.HttpTimeout
+import io.ktor.client.request.get
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 @AndroidEntryPoint
 class ChooseAppFragment : Fragment() {
@@ -27,13 +35,26 @@ class ChooseAppFragment : Fragment() {
     ): View {
         mBinding = FragmentChooseAppBinding.inflate(layoutInflater)
         mBinding.chooseAppButton.setOnClickListener {
+            mBinding.nextButton.isEnabled = false
             val chooseApp = ChooseAppDialog { app ->
                 mBinding.appName.text = app.name
                 mApp = app
+                runBlocking {
+                    if (isOnFdroid(app)) {
+                        Toast.makeText(
+                            context,
+                            getString(R.string.select_application_error),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    } else {
+                        mBinding.nextButton.isEnabled = true
+                    }
+                }
             }
             chooseApp.show(parentFragmentManager, "")
         }
 
+        mBinding.nextButton.isEnabled = false
         mBinding.nextButton.setOnClickListener { onNextClicked() }
         mBinding.backButton.setOnClickListener {
             findNavController().navigate(R.id.action_chooseAppFragment_to_warningFragment)
@@ -43,18 +64,22 @@ class ChooseAppFragment : Fragment() {
     }
 
     private fun onNextClicked() {
-        runBlocking {
-            if (mApp == null) {
-                Toast.makeText(
-                    context,
-                    getString(R.string.select_application_error),
-                    Toast.LENGTH_SHORT
-                ).show()
-                return@runBlocking
-            }
+        val bundle = bundleOf("package" to mApp?.packageName)
+        findNavController().navigate(R.id.action_chooseAppFragment_to_evaluateFragment, bundle)
+    }
 
-            val bundle = bundleOf("package" to mApp?.packageName)
-            findNavController().navigate(R.id.action_chooseAppFragment_to_evaluateFragment, bundle)
+    private suspend fun isOnFdroid(app: InstalledApplication): Boolean {
+        val client = HttpClient(Android) {
+            install(HttpTimeout)
+        }
+
+        return withContext(Dispatchers.IO) {
+            try {
+                client.get<String>("https://f-droid.org/api/v1/packages/${app.packageName}")
+                true
+            } catch (_: Exception) {
+                false
+            }
         }
     }
 }
