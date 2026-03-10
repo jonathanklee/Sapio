157a3c05cd845f511703ffd2f94a576a849eaf3a
Jonathan Klee <jonathan.klee@e.email>
2026-03-10 08:07:24 +0100

    fix: remove overlap between package name and icon in share image

--- app/src/main/java/com/klee/sapio/ui/view/ShareComposable.kt
+++ app/src/main/java/com/klee/sapio/ui/view/ShareComposable.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -116,7 +117,7 @@ fun ShareScreenshot(
                 Column(
                     modifier = Modifier
                         .align(Alignment.Center)
-                        .padding(start = 6.dp),
+                        .padding(horizontal = 48.dp),
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Text(
@@ -131,7 +132,9 @@ fun ShareScreenshot(
                         style = TextStyle(
                             color = Color.White.copy(alpha = 0.65f),
                             fontSize = 5.sp
-                        )
+                        ),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
                     )
 
                     Spacer(modifier = Modifier.height(6.dp))
